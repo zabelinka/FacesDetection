887b82fa1b753ac19445833961a161f532ffa373
tatiana <zabelina-tanya@mail.ru>
2016-12-12 23:57:32 +0300

    simple web app for faces detecting

---
+++ FacesDetectionWebApp/default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="FasesDetectionWebApp._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+            <div>
+                <h4>Select a file to upload:</h4>
+
+                <asp:FileUpload ID="FileUploadControl" runat="server"></asp:FileUpload>
+                <br />
+                <br />
+
+                <asp:Button ID="UploadButton" Text="Upload file" OnClick="UploadButton_Click" runat="server">
+                </asp:Button>
+
+                <hr />
+
+                <asp:Label ID="UploadStatusLabel" runat="server"></asp:Label>
+                <br />
+                <asp:Image ID="UploadedImage" runat="server" Width="500" ImageUrl="~/Images/Cat.jpg"></asp:Image>
+
+            </div>
+        </form>
+</body>
+</html>
---
+++ FacesDetectionWebApp/default.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Cvb;
+using Emgu.Util;
+
+namespace FasesDetectionWebApp 
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+            if (FileUploadControl.HasFile)
+            {    
+                String fileName = "image.jpg";
+                try
+                {
+                    FileUploadControl.SaveAs(Server.MapPath("Data") + "\\" + fileName);
+                    UploadStatusLabel.Text = "File " + fileName + " uploaded!";
+                    UploadedImage.ImageUrl = "~/Data/" + fileName;
+
+                    Image<Bgr, byte> img = DetectFaces(fileName);
+
+                    // display result
+                    img.Save(Server.MapPath("Data") + "\\result.jpg");
+                    UploadedImage.ImageUrl = "~/Data/result.jpg";
+                }
+                catch (Exception ex)
+                {
+                    UploadStatusLabel.Text = "The file could not be uploaded. The following error occured: " + ex.Message;
+                }  
+            }
+        }
+
+        private Image<Bgr, byte> DetectFaces(String fileName)
+        {
+            Image<Bgr, byte> img = new Image<Bgr, byte>(Server.MapPath("Data") + "\\" + fileName);
+
+            try
+            {
+                CascadeClassifier faceCascade = new CascadeClassifier(Server.MapPath("Data") + "\\haarcascade_frontalface_default.xml");
+                CascadeClassifier eyeCascade = new CascadeClassifier(Server.MapPath("Data") + "\\haarcascade_eye.xml");
+
+                Image<Gray, Byte> grayImage = img.Convert<Gray, Byte>();
+
+                var face_rectangles = faceCascade.DetectMultiScale(grayImage);
+
+                // drow found rectangles on image
+                foreach (var face in face_rectangles)
+                {
+                    img.Draw(face, new Bgr(255, 255, 255), 3);
+                }
+
+                // the same for eyes
+                var eye_rectangles = eyeCascade.DetectMultiScale(grayImage);
+                foreach (var eye in eye_rectangles)
+                {
+                    img.Draw(eye, new Bgr(0, 0, 255), 2);
+                }
+            }
+            catch (Exception ex)
+            {
+                UploadStatusLabel.Text = "Faces could not be detected. The following error occured: " + ex.Message;
+            }
+
+            return img;
+        }
+    }
+}
